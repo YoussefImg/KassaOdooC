b848af477905818c631a5ab0e8e67c6b28189a86
YoussefImg <img.youssef@gmail.Com>
2017-05-03 09:06:54 +0200

    Files geupload

---
+++ IPKassa/src/rabbitMq/Send.java
@@ -0,0 +1,30 @@
+package rabbitMq;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+
+public class Send {
+	  private final static String QUEUE_NAME = "hello kassa";
+
+	  public static void main(String[] argv)
+	      throws java.io.IOException, TimeoutException {
+		  ConnectionFactory factory = new ConnectionFactory();
+		  factory.setHost("10.3.51.32");
+		  factory.setUsername("kassa");
+		  factory.setPassword("Student1");
+		  Connection connection = factory.newConnection();
+		  Channel channel = connection.createChannel();
+		  
+		  channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		  String message = "Hello World!";
+		  channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		  System.out.println(" [x] Sent '" + message + "'");
+		  
+		  channel.close();
+		  connection.close();
+	  }
+	} 
